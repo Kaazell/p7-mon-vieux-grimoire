572110fa782527a7bce3cf589602e1e9dea3abc3
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-03 10:47:23 -0400

    CORS handle + get book hard coded for test

--- app.js
+++ app.js
@@ -3,22 +3,39 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("Requête reçue !");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
+app.use("/api/books", (req, res, next) => {
+  const books = [
+    {
+      userId: "oeihfzeoi",
+      title: "Mon premier objet",
+      author: "ATEF",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      year: 4900,
+      genre: "Fantasie",
+      genre: "String - genre du livre",
+      ratings: [
+        {
+          userId: "mongoId",
+          grade: 4,
+        },
+      ],
+      averageRating: 3,
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
